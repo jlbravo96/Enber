f4112ce4b94f3ee8267e1b5a4f1b7a83f51c6c1d
José Bravo <joseluis.bravo96@hotmail.com>
2021-02-09 21:31:19 -0300

    se creo el slide de las imagenes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* |||| |||| ---- MENU INTERACTION ---- |||| |||| */
+/* |||| |||| ---- MENU ---- |||| |||| */
 
 const $menuIcon = document.getElementById("menuIcon"),
   $pageNav = document.getElementById("pageNav"),
@@ -14,3 +14,29 @@ $menuIcon.addEventListener("click", function () {
 
   $body.classList.toggle("overflow-y-hidden");
 });
+
+/* |||| |||| ---- SLIDE ---- |||| |||| */
+
+const $slideImage = document.getElementById("slideImage");
+
+let i = 0,
+  images = [],
+  time = 2000;
+
+images[0] = "assets/images/slide-img1.webp";
+images[1] = "assets/images/slide-img2.webp";
+images[2] = "assets/images/slide-img3.webp";
+
+function changeImg() {
+  $slideImage.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+changeImg();
